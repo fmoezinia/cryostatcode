4a4db89e2c16b764327a71084d47fb470976331b
fmoezinia <moezinia.r@gmail.com>
2015-06-10 11:10:45 -0700

    Html webpage on local server

---
+++ practice.py
@@ -0,0 +1,16 @@
+<html>
+<!--(to run every x minutes using cron software)-->
+<head>
+<title> States Of Cryosystems</title>
+<meta http-equiv="refresh" content="500" />	
+</head>
+<body>
+<h1> Refer to Links below to view 24 Hour Plots displaying Cryosystem State <br> <br> Red = Error &nbsp;&nbsp;&nbsp;&nbsp 
+ Black = Inoperative &nbsp;&nbsp;&nbsp;&nbsp   Green = Operational &nbsp;&nbsp;&nbsp;&nbsp Yellow = Caution within 5 Hours</h1>
+
+<table border=1 bgcolor=#808082 style="width:90%"> 
+<tr>
+    <th>Antenna Number</th>
+    <th>Monitoring</th> 
+    <th>Image Link</th>
+  </tr> 
